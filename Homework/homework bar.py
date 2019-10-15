6f341b4e0689aa63e50e8e05f5bc906aeb339ea2
Alput22 <55756884+Alput22@users.noreply.github.com>
2019-10-15 12:45:14 +0700

    Add files via upload

---
+++ Homework/homework bar.py
@@ -0,0 +1,23 @@
+#%%
+import matplotlib.pyplot as plt
+filename = r'C:\Users\hp\Desktop\Mada\Homework\new\randomly.txt'
+strdict = {}
+with open(filename) as f:
+    fileread = f.read()
+    strings = fileread.split()
+    for word in strings:
+        if word in strdict:
+            strdict[word] += 1
+        else:
+            strdict[word] = 1
+
+counting = strdict.values()
+indentation = list(range(1, len(counting) + 1))
+plt.barh(indentation, counting, align='center', color=[0.6,0.3,0.2,0.9])
+plt.ylabel("Word list")
+plt.xlabel("Number of words")
+plt.title("Word counting")
+plt.yticks(indentation, strdict.keys())
+plt.show()
+
+print(strdict)
