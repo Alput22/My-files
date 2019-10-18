8b7c4283f1819fdc30cb8cfd6b8c56c354bed59b
Alput22 <55756884+Alput22@users.noreply.github.com>
2019-10-18 22:26:27 +0700

    Add files via upload

---
+++ Mr.Jude Homework/bank.py
@@ -0,0 +1,111 @@
+#%%
+class Account:
+    _balance = 0
+    
+    def __init__(self, balance2):
+        self._balance = balance2
+    
+    def getbalance(self):
+        return self._balance
+   
+    def deposit(self,amt):
+        if amt > 0:
+            self._balance = self._balance + amt
+            return True
+        else:
+            return False
+        
+    def withdraw(self,amt):
+        try:
+            if amt <= 0:
+                return False
+            else:
+                self._balance = amt
+                return True
+        except TypeError:
+            return False
+        
+myAccount = Account(5000)
+
+if (myAccount.deposit(10000)):
+    print("Account balance is >>",myAccount.getbalance())
+        
+class Customer:
+    _firstname = ""
+    _lastname = ""
+    _account = Account(5000)
+    
+    def __init__(self,firstname,lastname):
+        self._firstname = firstname
+        self._lastname = lastname
+    
+    def getfname(self):
+        return self._firstname
+    
+    def getlname(self):
+        return self._lastname
+    
+    def getaccount(self):
+        return self._account
+    
+    def setaccount(self, acct):
+        self._account = acct
+
+class Bank:
+    _customers = []
+    _number_of_customers = 0
+    _bankname = ""
+    
+    def __init__(self,customers,numberofcustomers,bankname):
+        self._customers = customers
+        self._number_of_customers = numberofcustomers
+        self._bankname = bankname
+    
+    def addcustomer(self,firstname,lastname):
+        if (firstname,lastname) not in self._customers:
+            self._customers.append(firstname + lastname)
+            self._number_of_customers += 1
+    
+    def getcustomer(self,i,int):
+        i = input("Please enter your name: ")
+        for i in self._customers:
+            print("hello", i)
+            
+    
+myAccount = Account(5000)
+
+if (myAccount.deposit(10000)):
+    print("Account balance is >>",myAccount.getbalance())
+
+myfcustomer = "Mia Ozawa"
+
+
+
+myBank = "Big OOO"
+        
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
+
+    
