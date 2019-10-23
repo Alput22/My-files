7cf2e9c2265fb68f2e6aaaa4505496bb284824b6
Alput22 <55756884+Alput22@users.noreply.github.com>
2019-10-23 17:23:51 +0700

    Update TOaO Quiz no.2.py

--- Mr.Jude Homework/TOaO Quiz no.2.py
+++ Mr.Jude Homework/TOaO Quiz no.2.py
@@ -6,8 +6,23 @@ Almada Putra A.
 filename = r'C:\Users\hp\Desktop\Mada\Homework\new\data.txt'
 
 class Display:
-    self.staff = list(filename.split("#"))
+    def __init__(self):
+        self.id = ID
+        self.name = Name
+        self.position = Pos
+        self.salary = Salary
+    @classmethod
     def display(self):
-        return self.staff
+        for da in open(filename):
+            data = da.rstrip().split('#')
+            print(data)
+    def choice():
+        choose = input("1.New Staff" +"\n2.Delete Staff" + "\n3.View Summary Data" + "\n4.Save & Exit" + "\ninput number: ")
+        
+    def conseq():
+        if (choose == 1):
+            Number.newstaff()
+Display.display()   
+Display.choice()
     
         
